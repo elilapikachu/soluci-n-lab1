bb232d0e17d1778e7a086c85fa6680eea4473b8f
Eliana Perez Ramos <eliana.perez@quipux.com>
2025-09-14 22:09:44 -0500

    Se soluciono segundo punto

--- src/Lista.java
+++ src/Lista.java
@@ -12,26 +12,26 @@ public class Lista {
     public Nodo primerNodo(){
         return this.primero;
     }
-//    public Nodo ultimoNodo(){
-//        return  this.ultimo;
-//    }
+    public Nodo ultimoNodo(){
+        return  this.ultimo;
+    }
 
-//    private Nodo anterior(Nodo nodo) {
-//        Nodo iterador = primerNodo();
-//        Nodo nodoAnt = null;
-//
-//        while (iterador != null && iterador != nodo) {
-//            nodoAnt = iterador;
-//            iterador = iterador.obtenerLiga();
-//        }
-//        return nodoAnt;
-//    }
+    private Nodo anterior(Nodo nodo) {
+        Nodo iterador = primerNodo();
+        Nodo nodoAnt = null;
+
+        while (iterador != null && iterador != nodo) {
+            nodoAnt = iterador;
+            iterador = iterador.obtenerLiga();
+        }
+        return nodoAnt;
+    }
 
     public Nodo buscaDondeInsertar(int candidato) {
         Nodo iterador = primerNodo();
         Nodo nodoAnt = null;
 
-        while (iterador != null && (int) iterador.obtenerDato() < candidato) {
+        while (iterador != null &&  iterador.obtenerDato() < candidato) {
             nodoAnt = iterador;
             iterador = iterador.obtenerLiga();
         }
@@ -55,7 +55,7 @@ public class Lista {
     }
 
     public void insertar(int dato) {
-        Nodo nodoAnterior = buscaDondeInsertar(dato);
+        Nodo nodoAnterior = ultimoNodo();
         Nodo nuevoNodo = new Nodo(dato);
         conectar(nuevoNodo, nodoAnterior);
     }
@@ -68,56 +68,90 @@ public class Lista {
         }
     }
 
-//    public Nodo buscarDato(int dato) {
-//        Nodo iterador = primerNodo();
-//
-//        while (iterador != null && (int) iterador.obtenerDato() != dato) {
-//            iterador = iterador.obtenerLiga();
-//        }
-//        return iterador;
-//    }
-//
-//
-//    public void desconectar(Nodo nodoADesconectar) {
-//        Nodo nodoAnterior = anterior(nodoADesconectar);
-//
-//        if (nodoADesconectar == primero) {
-//            primero = primero.obtenerLiga();
-//            if (primero == null) {
-//                ultimo = null;
-//            }
-//        } else {
-//            nodoAnterior.asignarLiga(nodoADesconectar.obtenerLiga());
-//            if (nodoADesconectar == ultimo) {
-//                ultimo = nodoAnterior;
-//            }
-//        }
-//    }
-//
-//    public void borrar(int datoABorrar) {
-//        Nodo nodoABorrar = buscarDato(datoABorrar);
-//        if (nodoABorrar == null) {
-//            throw new RuntimeException("Dato inexistente en la lista");
-//        }
-//        desconectar(nodoABorrar);
-//    }
+    public Nodo buscarDato(int dato) {
+        Nodo iterador = primerNodo();
+
+        while (iterador != null && (int) iterador.obtenerDato() != dato) {
+            iterador = iterador.obtenerLiga();
+        }
+        return iterador;
+    }
+
+
+    public void desconectar(Nodo nodoADesconectar) {
+        Nodo nodoAnterior = anterior(nodoADesconectar);
+
+        if (nodoADesconectar == primero) {
+            primero = primero.obtenerLiga();
+            if (primero == null) {
+                ultimo = null;
+            }
+        } else {
+            nodoAnterior.asignarLiga(nodoADesconectar.obtenerLiga());
+            if (nodoADesconectar == ultimo) {
+                ultimo = nodoAnterior;
+            }
+        }
+    }
+
+    public void borrar(int datoABorrar) {
+        Nodo nodoABorrar = buscarDato(datoABorrar);
+        if (nodoABorrar == null) {
+            throw new RuntimeException("Dato inexistente en la lista");
+        }
+        desconectar(nodoABorrar);
+    }
 
     public void ordenarMenorAMayor() {
         if (primerNodo() == null || primerNodo().obtenerLiga() == null) {
-            throw new RuntimeException("La lista esta vacia o tiene un solo elemento");
+            throw new RuntimeException("La lista está vacía o tiene un solo elemento");
         }
+
         Nodo iterador = primerNodo();
         while (iterador != null) {
-            Nodo mayor = iterador;
-            Nodo aux = iterador.obtenerLiga();
-            while (aux != null) {
-                if ( aux.obtenerDato() > mayor.obtenerDato()) {
-                    mayor = aux;
-                    System.out.println("Mayor actual: " + mayor.obtenerDato());
+            Nodo mayor = iterador.obtenerLiga();
+            Nodo menor = iterador;
+
+            while (mayor != null) {
+                if (mayor.obtenerDato() < menor.obtenerDato()) {
+                    menor = mayor;
                 }
-                aux = aux.obtenerLiga();
+                mayor = mayor.obtenerLiga();
             }
+
+            if (menor != iterador) {
+                int temp = iterador.obtenerDato();
+                iterador.asignarDato(menor.obtenerDato());
+                menor.asignarDato(temp);
+            }
+
             iterador = iterador.obtenerLiga();
         }
+    }
+
+
+    public void eliminarNodosDominados() {
+        if (primerNodo() == null || primerNodo().obtenerLiga() == null) {
+            throw new RuntimeException("La lista esta vacia o tiene un solo elemento");
+        }
+
+        Nodo aux = primerNodo();
+        Nodo iterador = aux.obtenerLiga();
+
+        while (iterador != null && aux != null) {
+            System.out.println("Iterador actual: " + iterador.obtenerDato());
+            System.out.println("Aux actual: " + aux.obtenerDato());
+            if( aux.obtenerDato() <  iterador.obtenerDato()){
+                System.out.println("Valor eliminado de la lista de nodos:  " + aux.obtenerDato());
+                desconectar(aux);
+                aux = iterador;
+            } else {
+                aux = aux.obtenerLiga();
+            }
+
+            iterador = iterador.obtenerLiga();
+        }
+
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         Lista lista = new Lista();
-        lista.insertar(5);
+        lista.insertar(12);
+        lista.insertar(20);
+        lista.insertar(7);
+        lista.insertar(65);
         lista.insertar(3);
-        lista.insertar(8);
-        lista.insertar(1);
         lista.insertar(4);
 
-        lista.ordenarMenorAMayor();
+       // lista.ordenarMenorAMayor();
+        lista.recorre();
+        lista.eliminarNodosDominados();
+        System.out.println("La lista despues de eliminar nodos eliminados:");
         lista.recorre();
     }
 }
